8aa609763fd1091137164330f295abf58de07a38
filipeomendes <filipedeoliveiramendes@gmail.com>
2023-09-09 19:37:36 -0300

    Importando as rotas para a main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,48 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter } from 'react-router-dom'
+import Login from './routes/Login.jsx'
+import Home from './routes/Home.jsx'
+import Maps from './routes/Maps.jsx'
+import Solicitacao from './routes/Solicitacao.jsx'
+import Status from './routes/Status.jsx'
+import Contato from './routes/Contato.jsx'
+import Error from './routes/Error.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/",
+        element: <Login/>
+      },
+      {
+        path: "/Home",
+        element: <Home/>
+      },
+      {
+        path: "/Maps",
+        element: <Maps/>
+      },
+      {
+        path: "/maps/solicitacao",
+        element: <Solicitacao/>
+      },
+      {
+        path: "/status",
+        element: <Status/>
+      },
+      {
+        path: "/contato",
+        element: <Contato/>
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
