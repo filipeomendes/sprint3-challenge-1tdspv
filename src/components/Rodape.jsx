f20a92ceb0ec4b7066113471e0d5e999f9b8cc15
fernandopaparelli <rm551022@fiap.com>
2023-09-10 17:48:46 -0300

    Ajustando Rodape e implementando no app jsx

---
+++ src/components/Rodape.jsx
@@ -0,0 +1,23 @@
+import logoFacebook from '../assets/facebook.png'
+import logoGithub from '../assets/github.png'
+import logoInstagram from '../assets/instagram.png'
+
+export default function Rodape(){
+
+    return(
+        <footer>
+            <section className="conteudo-rodape">
+                <div className="redes-sociais-logos">
+                    <nav className="redes-sociais-nav">
+                        <a href="https://github.com/filipeomendes/sprint3-challenge-1tdspv"><img src={logoGithub} alt="Logo preto do Github" /></a>
+                        <a href="https://www.instagram.com/complex.wrld/"><img src={logoInstagram} alt="Logo preto do Instagram" /></a>
+                        <a href="https://www.facebook.com"> <img src={logoFacebook} alt="Logo preto do Facebook" /></a>
+                    </nav>
+                </div>
+                <div className='copyright'>
+                    <p className='texto-footer'>Todos os direitos reservados ©2023 - Complex</p>
+                </div>
+            </section>
+        </footer>
+    )
+}
