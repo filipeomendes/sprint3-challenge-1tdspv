7e11677ee6aac6389aec2a6369a3148e51a18d2d
fernandopaparelli <rm551022@fiap.com>
2023-09-10 21:13:17 -0300

    Adicionando tag main nas rotas

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom"
 export default function Error() {
     document.title = "Error 404";
     return (
-      <div>
-          <h1>ERROR 404 - PÁGINA NÃO EXISTENTE!</h1>
-          <h2>Clique <Link to="/home">AQUI</Link> para voltar ao início. </h2>
-      </div>
+      <main>
+        <div>
+            <h1>ERROR 404 - PÁGINA NÃO EXISTENTE!</h1>
+            <h2>Clique <Link to="/home">AQUI</Link> para voltar ao início. </h2>
+        </div>
+      </main>
     )
   }
--- src/routes/Solicitacao.jsx
+++ src/routes/Solicitacao.jsx
@@ -1,8 +1,10 @@
 export default function Solicitacao() {
     document.title = "Solicitacao";
     return (
-      <div className="titulo">
-          <h1>Solicitar Resgate</h1>
-      </div>
+      <main>
+        <div className="titulo">
+            <h1>Solicitar Resgate</h1>
+        </div>
+      </main>
     )
   }
